1a20db5eb7892572e8766540216f0cf8da03494c
HoDangTai <152425828+HoDangTai@users.noreply.github.com>
2024-01-20 11:36:02 +0700

    bai1

---
+++ Bai1_Hello/src/SayHelloApp.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao, em la Ho Dang Tai SV K63CNTT - NTU: Hoc lap trinh Java");
+	}
+
+}
